3d1e8d186fd7e3fe2f883547aad507bf3af1a1ff
nikitha <gullapalli.nikitha@gmail.com>
2020-11-20 22:33:43 -0800

    Initial commit

---
+++ app/src/main/java/com/example/examples_gradle_commonusecases/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.examples_gradle_commonusecases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.examples_gradle_commonusecases.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
